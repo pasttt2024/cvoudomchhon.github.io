218f9d98f10d210091888d44e2c15037b8ddac43
Past <oudomchhon9@gmail.com>
2025-03-03 05:52:10 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     const links = document.querySelectorAll('a[href^="#"]');
+
+//     links.forEach((link) => {
+//       link.addEventListener("click", function (event) {
+//         event.preventDefault();
+//         const targetId = this.getAttribute("href").substring(1);
+//         const targetElement = document.getElementById(targetId);
+
+//         if (targetElement) {
+//           window.scrollTo({
+//             top: targetElement.offsetTop - 20, // Adjust the offset if needed
+//             behavior: "smooth",
+//           });
+//         }
+//       });
+//     });
+//   });
